2e8a8f41db386ebae409024f16fa629ffc4ec2a4
LukaszKor <lucaskor1997@gmail.com>
2015-11-27 16:42:52 +0000

    Movement Basics

---
+++ Assets/MovementBasic.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementBasic : MonoBehaviour {
+
+
+    public GameObject bullet; 
+    float speed = 6f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+            print("Left arrow was pressed");
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)){
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            print("Right arrow was pressed");
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+
+
+        }
+
+    }
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+    public int speed = 4;
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+
+    rb = gameObject.GetComponent<Rigidbody2D>();
+        // rigidbody2D.velocity.y = speed;
+        rb.MovePosition(rb.position * speed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    
+
+}
