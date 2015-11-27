935bd8948690a33e9ce92214f66eaae5679e4fc6
rebexe <rwalker066@gmail.com>
2015-11-27 16:22:04 +0000

    Initial Commit

---
+++ Assets/Exit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+	// Use this for initialization
+
+
+    void Start()
+    {
+      //  Debug.Log("fgvdsf");
+    }
+
+    public void ExitButton()
+    {
+        Debug.Log("sfsffv");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
