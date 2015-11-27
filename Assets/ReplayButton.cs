6849dcffd2441f5b0fb7012c335d686de2b4bc6b
rebexe <rwalker066@gmail.com>
2015-11-27 17:02:41 +0000

    GUIMenus
    Done GUI menus with scripts

---
+++ Assets/ReplayButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // Debug.Log("blah blah");
+    }
+
+    public void PlayButton()
+    {
+        Debug.Log("blah blah");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
